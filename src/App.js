f24e6b4afbf33c825e2012ca75fb8fc99b6c3a3f
Den4icGuti <goos7412@gmail.com>
2023-04-28 22:31:59 +0300

    install react

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Деплой приложения</p>
+        <h3>Deploy application</h3>
+
         <a
           className="App-link"
           href="https://reactjs.org"
