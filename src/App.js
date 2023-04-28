e10b11717e1409c22add9afa5d8de37991cb2f9e
Den4icGuti <goos7412@gmail.com>
2023-04-28 23:29:04 +0300

    add component container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { nanoid } from "nanoid";
+import { Container } from "./components/container";
 const bd = [
   { id: nanoid(), name: "Jack", email: "jk@gmail.com", phone: "11458744" },
   { id: nanoid(), name: "Max", email: "mx@gmail.com", phone: "1741225" },
@@ -7,7 +8,11 @@ const bd = [
 
 console.log(bd);
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Container />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/container.jsx
@@ -0,0 +1,14 @@
+import styles from "./container.module.css";
+
+export const Container = () => {
+  return (
+    <div className={styles.container}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, sunt
+        quasi! Minima repellendus eveniet ipsam dolorem facilis nulla quaerat
+        rerum vero laborum, iste enim voluptatibus? Suscipit architecto et ab
+        quos.
+      </p>
+    </div>
+  );
+};
